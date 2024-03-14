10ddb4d76cb3245bab5480498e9597a1200d07e5
JamesCroissant <yuham1014@gmail.com>
2024-03-13 21:13:45 -0700

    add the routing for each page

--- react-midterm-project/src/App.tsx
+++ react-midterm-project/src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Home from './components/home/Home'
+import ScoreBoard from './components/scoreboard/ScoreBoard'
+import Standings from './components/standings/Standings'
+import News from './components/news/News'
+import NotFound from './components/notfound/NotFound'
 
 function App() {
 
   return (
     <>
-      <h1>hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="scoreboard" element={<ScoreBoard />} />
+            <Route path="standings" element={<Standings />} />
+            <Route path="news" element={<News />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
