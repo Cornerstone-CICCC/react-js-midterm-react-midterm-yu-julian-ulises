e17f7fcdc99b09be9730a59f5f2251e6f2cc8d1c
Ulises Andrade <ulisesav25@gmail.com>
2024-03-16 12:03:30 -0700

    component files

--- react-midterm-project/src/pages/Standings/Standings.tsx
+++ react-midterm-project/src/pages/Standings/Standings.tsx
@@ -1,22 +1,21 @@
-import  { useEffect, useState } from 'react';
-import { Container, Table } from 'react-bootstrap';
-import teamBadges from '../../utils/teamBadges';
+import { useEffect, useState } from 'react';
+import Standingslogo from "../../components/TableStandings/logostanding";
+import StandingsTable from "../../components/TableStandings/standingtable";
 import './Standings.css';
-import leagueLogo from '../../assets/Images_badge/england-premier-league-logo-on-transparent-background-free-vector.jpg';
 
 type StandingsRow = {
   rank: string;
   team_badge?: string;
   name: string;
   points: string;
-  goals_scored: string; // GA
-  goals_conceded: string; // GC
-  goal_diff: string; // DIF
-  matches: string; // MP
-  won: string; // W
-  drawn: string; // D
-  lost: string; // L
-}
+  goals_scored: string;
+  goals_conceded: string;
+  goal_diff: string;
+  matches: string;
+  won: string;
+  drawn: string;
+  lost: string;
+};
 
 const Standings = () => {
   const [standings, setStandings] = useState<StandingsRow[]>([]);
@@ -43,55 +42,10 @@ const Standings = () => {
   }, [apiKey, secretKey]);
 
   return (
-    <div className="container mt-5">
-      <div className="league-info">
-        <img src={leagueLogo} alt="Premier League Logo" className="league-logo" />
-        <div>
-          <h2 className="league-title">ENGLISH PREMIER LEAGUE</h2>
-          <p className="league-description">Top four teams qualify for next season's Champions League</p>
-          <p className="league-description">Fifth place team qualifies for next season's Europa League</p>
-          <p className="league-description">Bottom three teams are relegated</p>
-        </div>
-      </div>
-
-      <Container className='PremierTable'>
-      <Table striped bordered hover variant="dark" className='PremierTable'>
-        <thead>
-          <tr>
-            <th>Position</th>
-            <th></th> 
-            <th>Team</th>
-            <th>MP</th>
-            <th>PTS</th>
-            <th>W</th>
-            <th>D</th>
-            <th>L</th>
-            <th>GF</th>
-            <th>GA</th>
-            <th>GD</th>
-          </tr>
-        </thead>
-        <tbody>
-          {standings.map((row, index) => (
-            <tr key={index}>
-              <td>{row.rank}</td>
-              <td>
-                <img src={teamBadges[row.name]} alt={`Escudo de ${row.name}`} style={{ width: '50px' }} />
-              </td>
-              <td>{row.name}</td>
-              <td>{row.matches}</td>
-              <td>{row.points}</td>
-              <td>{row.won}</td>
-              <td>{row.drawn}</td>
-              <td>{row.lost}</td>
-              <td>{row.goals_scored}</td>
-              <td>{row.goals_conceded}</td>
-              <td>{row.goal_diff}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table></Container>
-    </div>
+    <>
+      <Standingslogo />
+      <StandingsTable standings={standings} />
+    </>
   );
 };
 
