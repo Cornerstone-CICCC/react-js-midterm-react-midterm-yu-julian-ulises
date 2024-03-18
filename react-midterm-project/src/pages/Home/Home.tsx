4c2c2404c7eee4c7a61ba15efac98fe699b93f57
JamesCroissant <yuham1014@gmail.com>
2024-03-18 11:14:56 -0700

    fix: change the position of logo section

--- react-midterm-project/src/pages/Home/Home.tsx
+++ react-midterm-project/src/pages/Home/Home.tsx
@@ -1,9 +1,19 @@
+import HeroSection from '../../components/HeroSection/HeroSection'
+import ScoreBoard from '../Scoreboard/ScoreBoard'
+
 import './Home.css'
 
 const Home = () => {
+
   return (
     <>
-      <div>home</div>
+      <HeroSection />
+      <h2>SCOREBOARD</h2>
+      <ScoreBoard/>
+      <h2>STANDINGS</h2>
+      {/* <StandingsTable /> */}
+      <h2>Ranking</h2>
+      {/* <RankingTable /> */}
     </>
   )
 }
