ae92c1ec79051756e3a96cc36e73ea947c0e5cb9
JamesCroissant <yuham1014@gmail.com>
2024-03-17 17:53:21 -0700

    add: create hero section component

---
+++ react-midterm-project/src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,17 @@
+import leagueLogo from '../../assets/Images_badge/england-premier-league-logo-on-transparent-background-free-vector.jpg';
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div className="herosection-info">
+      <img src={leagueLogo} alt="Premier League Logo" className="herosection-logo" />
+      <div>
+        <h2 className="herosection-title">ENGLISH PREMIER LEAGUE</h2>
+        <p className="herosection-description">This App is for checking the data of premier league!</p>
+        <p className="herosection-description">We plan to publish information on other European leagues in the future!</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
